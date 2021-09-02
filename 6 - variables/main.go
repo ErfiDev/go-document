55a14bc25084fa07ae1e146b9eaa90ccca9c20f9
Erfan <erfanhanifezade@gmail.com>
2021-09-02 22:32:40 +0430

    variable section complete

---
+++ 6 - variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// has two way to declare a variable in go language
+
+	// first
+	var name string = "erfan"
+	var age = 17
+
+	// second
+	email := "erfanhanifezade@gmail.com"
+
+	fmt.Println("my name is: ", name)
+	fmt.Println("my age: ", age)
+	fmt.Println("and my email: ", email)
+}
