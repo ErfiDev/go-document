d7ba04ec4d8a15fa946af021eabb3bd35423fce7
Erfan <erfanhanifezade@gmail.com>
2021-09-02 15:37:00 +0430

    values in golang complete

---
+++ 5 - values/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Data types in golang
+func main() {
+	// Integer types
+	var age int = 27
+
+	// Floating types
+	var pi float64 = 3.14
+
+	// String types
+	var name string = "erfan"
+
+	// Boolean types
+	var human bool = true
+
+	fmt.Println("my age: ", age)
+	fmt.Println("my name: ", name)
+	fmt.Println("im a human? ", human)
+	fmt.Println("pi number: ", pi)
+}
