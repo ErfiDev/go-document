3d5bc0b0dfa5a08b28061a7db26411ed5b570ed9
Erfan <erfanhanifezade@gmail.com>
2021-10-11 21:25:11 +0330

    complete recover section

--- 25 - recover/main.go
+++ 25 - recover/main.go
@@ -2,22 +2,31 @@ package main
 
 import "fmt"
 
+func mayPanic() {
+	panic("a problem")
+}
+
 func main() {
-	// Go makes it possible to recover from a panic,
-	// by using the recover built-in function. A recover
-	// can stop a panic from aborting the program and
-	// let it continue with execution instead.
+	// Go makes it possible to recover from a panic, by
+	// using the recover built-in function. A recover
+	// can stop a panic from aborting the program and let
+	//  it continue with execution instead.
 
+	// An example of where this can be useful: a server
+	// wouldn’t want to crash if one of the client
+	// connections exhibits a critical error. Instead,
+	// the server would want to close that connection and
+	// continue serving other clients. In fact, this is
+	// what Go’s net/http does by default for HTTP servers.
 
-	defer func () {
-		if r := recover() ; r != nil {
-			fmt.Printf("this error: %s" , r)
+	defer func() {
+		if r := recover(); r != nil {
+
+			fmt.Println("Recovered. Error:\n", r)
 		}
 	}()
 
-	my()
-}
+	mayPanic()
 
-func my(){
-	panic("test panic")
+	fmt.Println("After mayPanic()")
 }
