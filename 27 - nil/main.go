063d7e2b9560c015d45bbef0e70cdb67f373f7ea
Erfan Hanifezade <73667127+ErfiDev@users.noreply.github.com>
2021-10-24 21:05:06 +0330

    Update main.go

--- 27 - nil/main.go
+++ 27 - nil/main.go
@@ -11,14 +11,20 @@ func main() {
 	// Coming from Java or other programming language
 	// background you might think they are but they
 	// partly are the same yet somewhat different.
-	// Coming from Java or other programming language
-	// background you might think they are but they
-	// partly are the same yet somewhat different.
+	// nil is a predeclared identifier in Go that 
+	// represents zero values for pointers, interfaces, 
+	// channels, maps, slices and function types. nil being 
+	// the zero value of pointers and interfaces, uninitialized 
+	// pointers and interfaces will be nil.So does that mean a 
+	//  string can’t be nil, yes you are right, a string doesn’t 
+	// qualify to be either of the above-mentioned types 
+	// and hence can’t be assigned nil.
 
 	// a := nil
-	// Coming from Java or other programming language
-	// background you might think they are but they
-	// partly are the same yet somewhat different.
+	// This will be a compile-time error use of untyped nilsince
+	// nil is untyped and the only untyped value that doesn’t 
+	// have default type and the compiler has no idea which 
+	// type it has to assign to a.
 
 	erfan := getPerson()
 	fmt.Println(erfan)
