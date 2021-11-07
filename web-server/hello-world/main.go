25036b241c61c24836841da25334adf6f8b9a504
Erfan <erfanhanifezade@gmail.com>
2021-11-07 23:05:56 +0330

    refactor folders

--- web-server/hello-world/main.go
+++ web-server/hello-world/main.go
@@ -5,7 +5,7 @@ import "net/http"
 // Basic Example of web servers
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("So Easy!"))
+		w.Write([]byte("Hello, world!"))
 	})
 
 	http.ListenAndServe(":3000", nil)
