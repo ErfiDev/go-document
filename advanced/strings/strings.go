6cf1112570566f805c13ce55288cca9e6e53a646
Erfan <erfanhanifezade@gmail.com>
2021-12-30 22:52:06 +0330

    complete advanced/strings section

--- advanced/strings/strings.go
+++ advanced/strings/strings.go
@@ -13,33 +13,50 @@ func main() {
 	myName := "Erfan"
 
 	// HasPrefix function tests whether the string s begins with prefix
-	fmt.Println(strings.HasPrefix(myName , "Erf"))
+	fmt.Println(strings.HasPrefix(myName, "Erf"))
 
 	// Contains function testing whether a string contain a substring
-	fmt.Println(strings.Contains(myName , "f"))
+	fmt.Println(strings.Contains(myName, "f"))
 
 	// Index and LastIndex functions
-	fmt.Println(strings.Index(myName , "n"))
-	fmt.Println(strings.LastIndex(myName , "v"))
+	fmt.Println(strings.Index(myName, "n"))
+	fmt.Println(strings.LastIndex(myName, "v"))
 
 	// Replacing string
-	fmt.Println(strings.Replace(myName , "fan" , "nan" , 2))
+	fmt.Println(strings.Replace(myName, "fan", "nan", 2))
 
 	// Count function:  return number of non-overlapping
 	forTest := "Erfaaaaaaaaan"
-	fmt.Println(strings.Count(forTest , "a"))
+	fmt.Println(strings.Count(forTest, "a"))
 
 	// Repeat function
-	fmt.Println(strings.Repeat("ba" , 2))
+	fmt.Println(strings.Repeat("ba", 2))
 
 	// Changing the case of the string
-	fmt.Println(strings.ToLower(myName) , strings.ToUpper(myName))
+	fmt.Println(strings.ToLower(myName), strings.ToUpper(myName))
 
 	// Trim functions
 	withSpace := "Er fa n"
 	trimLeftStr := "johnjohn"
-	fmt.Println(strings.Trim(myName , "Er"))
+	fmt.Println(strings.Trim(myName, "Er"))
 	fmt.Println(strings.TrimSpace(withSpace))
-	fmt.Println(strings.TrimLeft(trimLeftStr , "jo"))
-	fmt.Println(strings.TrimRight(trimLeftStr , "hn"))
+	fmt.Println(strings.TrimLeft(trimLeftStr, "jo"))
+	fmt.Println(strings.TrimRight(trimLeftStr, "hn"))
+
+	// Splitting a string
+	// - on whitespace
+	fmt.Println(strings.Fields(myName))
+
+	withWhiteSpace := "E r f an"
+	fmt.Println(strings.Fields(withWhiteSpace))
+
+	// - on a seprator
+	fmt.Println(strings.Split(myName, ""))
+
+	withSep := "E*rfa*n"
+	fmt.Println(strings.Split(withSep, "*"))
+
+	// Joining sliced string with Join method
+	strSlice := strings.Split(myName, "")
+	fmt.Println(strings.Join(strSlice, ""))
 }
