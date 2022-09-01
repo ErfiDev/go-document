41ec3982bfba4bd50cd55f75fa53e16fdb0a80fe
erfidev <erfanhanifezade@gmail.com>
2022-09-01 18:06:28 +0430

    sleeping barber concurrency problem

---
+++ advanced/concurrency/sleeping_barber/sleeping_barber.go
@@ -0,0 +1,180 @@
+// This is a simple demonstration of how to solve the Sleeping Barber dilemma, a classic computer science problem
+// which illustrates the complexities that arise when there are multiple operating system processes. Here, we have
+// a finite number of barbers, a finite number of seats in a waiting room, a fixed length of time the barbershop is
+// open, and clients arriving at (roughly) regular intervals. When a barber has nothing to do, he or she checks the
+// waiting room for new clients, and if one or more is there, a haircut takes place. Otherwise, the barber goes to
+// sleep until a new client arrives. So the rules are as follows:
+//
+//		- if there are no customers, the barber falls asleep in the chair
+//		- a customer must wake the barber if he is asleep
+//		- if a customer arrives while the barber is working, the customer leaves if all chairs are occupied and
+//		  sits in an empty chair if it's available
+//		- when the barber finishes a haircut, he inspects the waiting room to see if there are any waiting customers
+//		  and falls asleep if there are none
+// 		- shop can stop accepting new clients at closing time, but the barbers cannot leave until the waiting room is
+//	      empty
+//		- after the shop is closed and there are no clients left in the waiting area, the barber
+//		  goes home
+//
+// The Sleeping Barber was originally proposed in 1965 by computer science pioneer Edsger Dijkstra.
+//
+// The point of this problem, and its solution, was to make it clear that in a lot of cases, the use of
+// semaphores (mutexes) is not needed.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+// variables
+var seatingCapacity = 10
+var arrivalRate = 100
+var cutDuration = 1000 * time.Millisecond
+var timeOpen = 10 * time.Second
+
+func main() {
+	// seed our random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// print welcome message
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
+
+	// create channels if we need any
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
+
+	// create the barbershop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
+
+	color.Green("The shop is open for the day!")
+
+	// add barbers
+	shop.addBarber("Frank")
+
+	// start the barbershop as a goroutine
+	shopClosing := make(chan bool)
+	closed := make(chan bool)
+
+	go func() {
+		<-time.After(timeOpen)
+		shopClosing <- true
+		shop.closeShopForDay()
+		closed <- true
+	}()
+
+	// add clients
+	i := 1
+
+	go func() {
+		for {
+			// get a random number with average arrival rate
+			randomMillseconds := rand.Int() % (2 * arrivalRate)
+			select {
+			case <-shopClosing:
+				return
+			case <-time.After(time.Millisecond * time.Duration(randomMillseconds)):
+				shop.addClient(fmt.Sprintf("Client #%d", i))
+				i++
+			}
+		}
+	}()
+
+	// block until the barbershop is closed
+	<-closed
+}
+
+type BarberShop struct {
+	ShopCapacity    int
+	HairCutDuration time.Duration
+	NumberOfBarbers int
+	BarbersDoneChan chan bool
+	ClientsChan     chan string
+	Open            bool
+}
+
+func (shop *BarberShop) addBarber(barber string) {
+	shop.NumberOfBarbers++
+
+	go func() {
+		isSleeping := false
+		color.Yellow("%s goes to the waiting room to check for clients.", barber)
+
+		for {
+			// if there are no clients, the barber goes to sleep
+			if len(shop.ClientsChan) == 0 {
+				color.Yellow("There is nothing to do, so %s takes a nap.", barber)
+				isSleeping = true
+			}
+
+			client, shopOpen := <-shop.ClientsChan
+
+			if shopOpen {
+				if isSleeping {
+					color.Yellow("%s wakes %s up.", client, barber)
+					isSleeping = false
+				}
+				// cut hair
+				shop.cutHair(barber, client)
+			} else {
+				// shop is closed, so send the barber home and close this goroutine
+				shop.sendBarberHome(barber)
+				return
+			}
+		}
+	}()
+}
+
+func (shop *BarberShop) cutHair(barber, client string) {
+	color.Green("%s is cutting %s's hair.", barber, client)
+	time.Sleep(shop.HairCutDuration)
+	color.Green("%s is finished cutting %s's hair.", barber, client)
+}
+
+func (shop *BarberShop) sendBarberHome(barber string) {
+	color.Cyan("%s is going home.", barber)
+	shop.BarbersDoneChan <- true
+}
+
+func (shop *BarberShop) closeShopForDay() {
+	color.Cyan("Closing shop for the day.")
+
+	close(shop.ClientsChan)
+	shop.Open = false
+
+	for a := 1; a <= shop.NumberOfBarbers; a++ {
+		<-shop.BarbersDoneChan
+	}
+
+	close(shop.BarbersDoneChan)
+
+	color.Green("---------------------------------------------------------------------")
+	color.Green("The barbershop is now closed for the day, and everyone has gone home.")
+}
+
+func (shop *BarberShop) addClient(client string) {
+	// print out a message
+	color.Green("*** %s arrives!", client)
+
+	if shop.Open {
+		select {
+		case shop.ClientsChan <- client:
+			color.Yellow("%s takes a seat in the waiting room.", client)
+		default:
+			color.Red("The waiting room is full, so %s leaves.", client)
+		}
+	} else {
+		color.Red("The shop is already closed, so %s leaves!", client)
+	}
+}
